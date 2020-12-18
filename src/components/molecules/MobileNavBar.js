516d107c5b7f54ba2ff719530b18efb0b552f77a
Preston Carlton <prescarlton@gmail.com>
2020-12-17 21:09:01 -0500

    more work on mobile nav, threw something together for a quick mobile lander

--- src/components/molecules/MobileNavBar.js
+++ src/components/molecules/MobileNavBar.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const MobileNavBar = () => {
 
     const [renderNavDrawer, setRenderNavDrawer] = useState(false);
 
     return (
-        <>
+        <TransitionGroup component={null}>
             {!renderNavDrawer ? (
                 <nav className='navbar--mobile'>
                     <ul className='navbar__linkGroup--mobile'>
@@ -15,22 +16,23 @@ const MobileNavBar = () => {
                         {/* <li onClick={() => { setRenderNavDrawer(true) }}>hi</li> */}
                         <AiOutlineMenu className='linkGroup__icon' onClick={() => { setRenderNavDrawer(true) }} />
                     </ul>
-
                 </nav>
             ) : (
-                    <div className='navbar__drawer'>
-                        <div className='drawer__topRow'>
-                            <AiOutlineClose className='navbar__drawer__icon' onClick={() => setRenderNavDrawer(false)} />
+                    <CSSTransition classNames="navbar__drawer" timeout={400}>
+                        <div className='navbar__drawer'>
+                            <div className='drawer__topRow'>
+                                <AiOutlineClose className='navbar__drawer__icon' onClick={() => setRenderNavDrawer(false)} />
+                            </div>
+                            <div className='navbar__drawer__linkGroup'>
+                                <NavLink activeClassName='active' onClick={() => setRenderNavDrawer(false)} to='/' exact>Home</NavLink>
+                                <NavLink activeClassName='active' onClick={() => setRenderNavDrawer(false)} to='/about'>About</NavLink>
+                                <NavLink activeClassName='active' onClick={() => setRenderNavDrawer(false)} to='/projects'>Projects</NavLink>
+                                <NavLink activeClassName='active' onClick={() => setRenderNavDrawer(false)} to='/resume'>Resume</NavLink>
+                            </div>
                         </div>
-                        <div className='navbar__drawer__linkGroup'>
-                            <NavLink activeClassName='active' onClick={() => setRenderNavDrawer(false)} to='/' exact>Home</NavLink>
-                            <NavLink activeClassName='active' onClick={() => setRenderNavDrawer(false)} to='/about'>About</NavLink>
-                            <NavLink activeClassName='active' onClick={() => setRenderNavDrawer(false)} to='/projects'>Projects</NavLink>
-                            <NavLink activeClassName='active' onClick={() => setRenderNavDrawer(false)} to='/resume'>Resume</NavLink>
-                        </div>
-                    </div>
+                    </CSSTransition>
                 )}
-        </>
+        </TransitionGroup>
 
 
     );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className='page'>
-            homepage
+        <div className='page home'>
+            <h1 className='site-header highlight'>hey,</h1>
+            <h3 className='site-subheader'>I'm Preston.</h3>
+            <p>Click that fancy lookin' button to check out what I'm working on. </p>
+            <NavLink className='button--solid highlight' to='/projects'>Projects</NavLink>
         </div>
     )
 }
