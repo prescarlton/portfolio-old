a4c3a487e2f07e716eb8d830e7efc760c5e60bba
Preston Carlton <prescarlton@gmail.com>
2021-02-17 19:36:06 -0500

    more work on projects page

--- src/components/atoms/ProjectCard.js
+++ src/components/atoms/ProjectCard.js
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import { FiGithub } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 const ProjectCard = (props) => {
     return (
         <div className='projectCard'>
-            <h1>{props.name}</h1>
-            <p>{props.description}</p>
+            <img className='projectCard__thumb' src={require(`./../../img/${props.id}_screenshot.png`).default} />
+            <div className='projectCard__content'>
+                <h1>{props.name}</h1>
+                <p>{props.description}</p>
+                <ul>
+                    {props.tags.map(tag => {
+                        return (
+                            <>
+                                <li>{tag}</li>
+                                {props.tags[props.tags.length - 1] != tag && (<li>&middot;</li>)}
+                            </>
+                        )
+                    })}
+                </ul>
+                <a target='_blank' href={`https://www.github.com/${props.links.github}`}>
+                    <FiGithub className='projectCard__icon' />
+                </a>
+            </div>
         </div>
     )
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,29 +4,60 @@ import ProjectCard from '../atoms/ProjectCard';
 const projectList = [
     {
         name: 'tido',
-        description: 'A super nifty productivity webapp with a React-native port currently in the works.'
+        id: 'tido',
+        description: 'A super nifty productivity webapp with a React-native port currently in the works.',
+        tags: [
+            'React.js',
+            'AWS'
+        ],
+        links: {
+            github: 'prescarlton/tido'
+        }
     },
     {
         name: 'Box-Set',
-        description: 'A tool to manage my super legal collection of movies/tv shows.'
+        id: 'boxset',
+        description: 'A tool to manage my super legal collection of movies/tv shows.',
+        tags: [
+            'Python'
+        ],
+        links: {
+            github: 'prescarlton/box-set'
+        }
     },
     {
         name: 'Vuedal',
-        description: 'A super simple/lightweight modal component for Vue.js'
+        id: 'vuedal',
+        description: 'A super simple/lightweight modal component for Vue.js',
+        tags: [
+            'Vue.js',
+            'Node.js',
+            'npm'
+        ],
+        links: {
+            github: 'prescarlton/vuedal'
+        }
     },
     {
         name: 'Top 100',
-        description: 'A Python script to get the top 100 movies of all time, based on rankings from 15+ popular media outlets. A Python script to get the top 100 movies of all time, based on rankings from 15+ popular media outlets'
+        id: 'top100',
+        description: 'A Python script to get the top 100 movies of all time, based on rankings from 15+ popular media outlets.',
+        tags: [
+            'Python'
+        ],
+        links: {
+            github: 'prescarlton/top-100'
+        }
     }
 ]
 
 const Projects = () => {
     return (
-        <div className='page'>
+        <div className='projects'>
             <div className='projectList'>
                 {
                     projectList.map(project => (
-                        <ProjectCard {...project} />
+                        <ProjectCard {...project} key={project.id}/>
                     ))
                 }
             </div>
